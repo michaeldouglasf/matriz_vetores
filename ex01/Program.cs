ff11b6370f470ce978e9b6698a9a2b4295247c6e
Michael Marcelino <michaeldougls93@gmail.com>
2026-01-21 10:46:45 +0000

    finish ex 21

--- ex01/Program.cs
+++ ex01/Program.cs
@@ -1,5 +1,5 @@
 ﻿namespace ex01;
- 
+
 class Program
 {
     static void Main(string[] args)
@@ -8,38 +8,35 @@ class Program
         int bedMaxAvg, hourMax, bedMax, maxValue;
         double[] avgPulses;
         double maxAvg, sum;
- 
-        pulses = new int[4, 4];
+
+        pulses = new int[24, 4];
         avgPulses = new double[4];
- 
-        Console.WriteLine("Insira as pulsacoes dos 4 pacientes ao longo de 4 horas:\n");
- 
-        for (int i = 0; i < 4; i++)
+
+        Console.WriteLine("Preenchendo as  pulsacoes  \n");
+
+        for (int i = 0; i < 24; i++)
         {
             for (int j = 0; j < 4; j++)
             {
-                do
-                {
-                    Console.Write($"Hora {i} - Cama {j + 1}: ");
-                } while (!int.TryParse(Console.ReadLine(), out pulses[i, j]) || pulses[i, j] < 0);
+                Console.WriteLine($"Hora {i} - Cama {j + 1}: {pulses[i, j]}");
             }
         }
- 
+
         for (int j = 0; j < 4; j++)
         {
             sum = 0;
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < 24; i++)
             {
                 sum = sum + pulses[i, j];
             }
-            avgPulses[j] = sum / 4;
+            avgPulses[j] = sum / 24;
         }
- 
+        Console.WriteLine($"\nMedias das camas");
         for (int j = 0; j < 4; j++)
         {
             Console.WriteLine($"Cama {j + 1}: {avgPulses[j]:F2}");
         }
- 
+
         maxAvg = avgPulses[0];
         bedMaxAvg = 1;
         for (int j = 1; j < 4; j++)
@@ -50,14 +47,14 @@ class Program
                 bedMaxAvg = j + 1;
             }
         }
- 
+
         Console.WriteLine($"\nPaciente com maior media: Cama {bedMaxAvg} ({maxAvg:F2})");
- 
+
         maxValue = pulses[0, 0];
         hourMax = 0;
         bedMax = 1;
- 
-        for (int i = 0; i < 4; i++)
+
+        for (int i = 0; i < 24; i++)
         {
             for (int j = 0; j < 4; j++)
             {
@@ -69,7 +66,7 @@ class Program
                 }
             }
         }
- 
+
         Console.WriteLine($"\nValor mais elevado: {maxValue} - Cama {bedMax} - Hora {hourMax}");
     }
 }
